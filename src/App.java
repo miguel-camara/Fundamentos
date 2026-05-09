c8b55766216e865de261a9f9bbff37d5905683af
Miguel Ordonez Camara <miguelordonezcamara@hotmail.com>
2026-05-09 15:31:43 -0600

    variables

--- src/App.java
+++ src/App.java
@@ -1,6 +1,26 @@
 public class App {
   public static void main(String[] args) throws Exception {
-    System.out.println("Hello, World!");
+
+  }
+
+  public static void variables() {
+    // Cadena
+    String cadena = "Cadena";
+    // Decimal
+    double decimalDouble = 10.25;
+    float decimalFloat = 20.44f;
+    // Enteros
+    byte enteroByte = 1;
+    short enteroShord = 10;
+    int entero = 100;
+    long enteroLong = 1000;
+    // Char
+    char caracter = 'A';
+    // Boolean
+    boolean esBoleano = true;
+    // var para asiganar cualquier tipo de forma dinamica pero se tiene que
+    // inicializar
+    var nombre = "Hola Mundo";
 
   }
 }
